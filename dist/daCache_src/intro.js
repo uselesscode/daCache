aac3c5dcdd180c6591b9fb27f7ab6ba59ce05e0d
uselesscode <i-stare@uselesscode.org>
2013-08-31 21:32:14 -0700

    Adding 1.0.0 distributable files

---
+++ dist/daCache_src/intro.js
@@ -0,0 +1,7 @@
+/*! daCache v1.0.0 2013-08-31
+ * Copyright 2013 Peter Johnson
+ * Licensed MIT, GPL-3.0
+ * https://github.com/uselesscode/daCache
+ */
+var daCache = (function ($) {
+  "use strict";
